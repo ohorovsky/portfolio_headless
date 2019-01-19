c097238c3da846a24ac6bf6c33fcb1ecaeda94f1
Ondrej Horovsky <o.horovsky@gmail.com>
2019-01-19 00:54:08 +0000

    final updates

--- src/components/MainBody/SmallFooter.js
+++ src/components/MainBody/SmallFooter.js
@@ -10,10 +10,10 @@ class SmallFooter extends Component {
                 <a href='https://www.facebook.com/ondrej.horovsky' title="Facebook" target="_blank" rel="noreferrer" className='small-nav__link'>
                     <Svg name="facebook" />
                 </a>
-                <a href='https://github.com/ohorovsky' title="Github" rel="noreferrer" className='small-nav__link'>
+                <a href='https://github.com/ohorovsky' title="Github" target="_blank" rel="noreferrer" className='small-nav__link'>
                     <Svg name="github" />
                 </a>
-                <a href='https://www.instagram.com/ohorovsky' title="Instagram" rel="noreferrer" className='small-nav__link'>
+                <a href='https://www.instagram.com/ohorovsky' title="Instagram" target="_blank" rel="noreferrer" className='small-nav__link'>
                     <Svg name="instagram" />
                 </a>
             </div>
